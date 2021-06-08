2fd2c7538e0652184995da6ec1df413fe0117cb2
Yatharth2001 <yatharthshah00@gmail.com>
2021-06-08 16:02:06 +0530

    Crud Operation

---
+++ Day 8 & Day 9/mysqlcrud/routes/index.js
@@ -0,0 +1,151 @@
+var express = require('express');
+var mysql      = require('mysql');
+var router = express.Router();
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'node-demo'
+});
+
+connection.connect(function(err){
+  if(!err){
+    console.log('Db connected');
+  }
+  else{
+    console.log('Db connection issue')
+  }
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/add', function(req, res, next) {
+  res.render('add-form', { title: 'Express' });
+});
+
+//insert rpute
+router.post('/add-process', function(req, res, next) {
+  console.log(req.body);
+  const mybodydata = {
+    product_name : req.body.txt1,
+    product_details : req.body.txt2,
+    product_price : req.body.txt3
+  }
+
+  connection.query("insert into tbl_product set ?",mybodydata,function(err,result){
+    if(err) throw err;
+    res.redirect('/add');
+  });
+
+});
+
+//display route
+router.get('/display', function(req, res, next) {
+  connection.query("select * from tbl_product",function(err,db_rows){
+    if(err) throw err;
+    console.log(db_rows);
+    res.render('display',{db_rows_array:db_rows});
+  });
+});
+
+//delete route
+router.get('/delete/:id', function(req, res, next) {
+  var deleteid = req.params.id;
+  console.log(deleteid);
+  connection.query("delete from tbl_product where product_id = ?",[deleteid],function(err,db_rows){
+    if(err) throw err;
+    console.log(db_rows);
+    res.redirect('/display');
+  });
+});
+
+//edit route
+router.get('/edit/:id', function(req, res, next) {
+  var editid = req.params.id;
+  console.log(editid);
+  connection.query("select * from tbl_product where product_id = ?",[editid],function(err,db_rows){
+    if(err) throw err;
+    console.log(db_rows);
+    res.render('edit',{db_rows_array:db_rows});
+  });
+});
+
+//Route Post
+router.post('/edit/:id', function(req, res, next) {
+  var editid = req.params.id;
+  var pname = req.body.txt1;
+  var pdetails = req.body.txt2;
+  var pprice = req.body.txt3;
+
+  connection.query("update tbl_product set product_name = ?,Product_details = ?,Product_price = ? where Product_id = ?",[pname,pdetails,pprice,editid],function(err,db_rows){
+    if(err) throw err;
+    res.redirect('/display');
+  });
+});
+module.exports = router;
+
+// var express = require('express');
+// var mysql      = require('mysql');
+// var router = express.Router();
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : '',
+//   database : 'node-demo'
+// });
+
+// connection.connect(function(err){
+//   if(!err){
+//     console.log("DB Connected");
+//   }else{
+//     console.log("DB Connection Issue");
+//   }
+// });
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+// router.get('/add', function(req, res, next) {
+//   res.render('add-form', { title: 'Express' });
+// });
+
+// //Insert process
+// router.post('/add-process', function(req, res, next) {
+//   console.log(req.body);
+
+//   const mybodyData={
+//     Product_name: req.body.txt1,
+//     Product_detais: req.body.txt2,
+//     Product_price: req.body.txt3,
+//   }
+
+//   connection.query("insert into tbl_product set ?",mybodyData,function(err,result){
+//     if(err) throw err;
+//     res.redirect('/add')
+//   });
+// });
+// //Display
+// router.get('/display', function(req, res, next) {
+  
+//   connection.query("select * from tbl_product ",function(err,db_rows){
+//     if(err) throw err;
+//     console.log(db_rows);
+//     res.render('display',{db_rows_array:db_rows});
+//   });
+// });
+// //delete Route
+// router.get('/delete/:id', function(req, res, next) {
+//   var deleteid = req.params.id;
+//   console.log(deleteid);
+//   connection.query("delete from tbl_product where product_id = ? ",[deleteid],function(err,db_rows){
+//     if(err) throw err;
+//     console.log(db_rows);
+//     res.redirect('/display');
+//   });
+// });
+
+
+// module.exports = router;
